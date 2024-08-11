ae78aa6909d68379092565c6509974d99366b69b
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2024-08-10 22:45:11 -0400

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,4 +1,5 @@
 from utils.layers import *
+from utils.schedulers import *
 from utils.network import Network
 from utils.optimizers import Adam
 from utils.functions import Activations, Loss
@@ -7,28 +8,16 @@ import numpy as np, pickle, time
 
 if __name__ == "__main__":
     model = [
-        Input((2, 7, 7)),
-        Conv2d(2, (3, 3)),
-        Activation("selu"),
-        Conv2d(2, (3, 3)),
-        Activation("selu"),
-        Conv2d(2, (3, 3)),
+        Input(2),
+        Dense(3),
+        Activation("lrelu"),
+        Dense(2),
         Activation("softmax"),
-        # BatchNorm(momentum = 0.9),
-        # Conv2d(2, (3, 3)),
-        # Activation("softmax"),
-        Flatten(),
-        # Dense(16),
-        # Activation("selu"),
-        # Dense(4),
-        # Activation("selu"),
-        # Dense(2),
-        # Activation("softmax"),
     ]
 
     print(model)
 
-    network = Network(model, loss_function="cross_entropy", optimizer=Adam(momentum = 0.9, beta_constant = 0.99, learning_rate=0.01))
+    network = Network(model, loss_function="cross_entropy", optimizer=Adam(momentum = 0.9, beta_constant = 0.99))
     network.compile()
     
     training_percent = 1
@@ -37,15 +26,14 @@ if __name__ == "__main__":
     save_file = 'model-training-data.json'
 
     xdata = [[i % 2, i // 2] for i in range(4)]
-    ydata = [[1 - (i % 2) ^ (i // 2), (i % 2) ^ (i // 2)] for i in range(4)]
+    ydata = [[(i % 2) ^ (i // 2), 1 - ((i % 2) ^ (i // 2))] for i in range(4)]
 
     costs = []
-    val_costs = []
     plt.ion()
 
     start_time = time.perf_counter()
 
-    for idx, cost in enumerate(network.fit(xdata, ydata, batch_size = batch_size, epochs = 400, threads=4)):
+    for idx, cost in enumerate(network.fit(xdata, ydata, learning_rate=0.01, batch_size = batch_size, epochs = 1000, threads=4)):
         if idx % 10:
             save_data = network.save()
 
@@ -56,18 +44,11 @@ if __name__ == "__main__":
 
         print(end_time - start_time, "time")
 
-        choice = np.random.randint(len(xdata))
-
-        model_output = network.forward(xdata[choice], training=False)
-        val_loss = network.loss_function(model_output, ydata[choice])
-
-        val_costs.append(val_loss)
         costs.append(cost)
 
         print(cost)
 
         plt.plot(np.arange(len(costs)) * (batch_size / (len(xdata) * training_percent)), costs, label='training')
-        plt.plot(np.arange(len(val_costs)) * (batch_size / (len(xdata) * training_percent)), val_costs, color='orange', label='validation')
 
         plt.legend()
         plt.draw()
